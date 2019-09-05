f0ccdc04436e434ae390258bb7081a071b4e8243
Bruno Ravanhani <bruno.ravanhani@sigma.com.br>
2019-09-05 13:22:35 -0400

    Estrutura inicial do projeto

---
+++ src/api/BookStore.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BookStore.Infra
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/api/BookStore.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BookStore.Service
+{
+    public class Class1
+    {
+    }
+}
